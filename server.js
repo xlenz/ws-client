841603c1a84f353cf22373ab721125bdbc139fb2
Mykola Grybyk <mgrybyk@serena.com>
2016-09-09 19:37:47 +0300

    fix issue with one socket

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ for (let i = 0; i < CONFIG.connections; i++) {
   (async () => {
     let authResponse = await auth();
     if (authResponse.success) {
-      if (i === 1) console.log('Logged in to SDA!');
+      if (i === 0) console.log('Logged in to SDA!');
       await ws(authResponse.cookies, i);
-    } else if (i === 1) {
+    } else if (i === 0) {
       console.error('Unable to login to SDA.');
       if (authResponse.error) console.error(authResponse.error);
       else console.log('Failed to authorize.');
